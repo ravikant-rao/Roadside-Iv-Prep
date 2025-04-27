b054eaf1ee2d8d99904ecd91e4acaae542d8f803
Ravikant <ravikantkumar707978@gmail.com>
2025-04-27 21:58:17 +0530

    ep-o7 completed

--- Roadside-Js/ep-07/script.js
+++ Roadside-Js/ep-07/script.js
@@ -68,3 +68,99 @@ for (let i = 0; i < animals.length; i++) {
     array.push.apply(array,elements);
     console.log(array);
     
+
+
+// * 4. Find min/max number in an array
+  const numbers = [4,5,6,7,8,9,3,2];
+  console.log(Math.max.apply(null, numbers));
+  console.log(Math.min.apply(null, numbers));
+
+
+
+// * 5. Bound Function
+  function f(){
+    console.log(this);
+    
+  }
+
+  let user = {
+    g: f.bind(null),
+  }
+
+  user.g();
+
+
+
+// * 6. Bind Chaining
+  function fn(){
+    console.log(this.name);
+    
+  }
+
+  fr = fn.bind({name: 'Sohan'}).bind({name: "Saurav"});
+  fr();
+
+
+
+// * 7. Fix the line  whr checkPassword is calling
+  function checkPassword(success,failed){
+    let password = prompt("paRRfssword?","");
+    if(password== "Coder") success();
+    else failed();
+  }
+
+  let user2 ={
+    name: "Piyush Singh",
+    loginSuccessful(){
+      console.log(`${this.name} logged in`);
+      
+    },
+
+    loginFailed(){
+      console.log(`${this.name} failed to log in`);
+      
+    },
+  };
+
+  // checkPassword(user.loginSuccessful, user.loginFailed);
+  // checkPassword(user2.loginSuccessful.bind(user2), user2.loginFailed.bind(user2));
+
+
+
+// * 8. Partial application for login function
+  function checkpassword(ok, fail){
+    let password = prompt("Password?", "");
+    if(password == "Coding") ok();
+    else fail();
+  }
+
+  let user3 = {
+    name: "Sakshi Priya",
+
+    login(result){
+      console.log(this.name + (result ? " login successful" : " login failed"));
+      
+    },
+  };
+  // ? checkpassword(?,?);
+  // checkpassword(user3.login.bind(user3,true), user3.login.bind(user3,false));
+
+
+
+// * 9 Explicit Binding with Arrow Function
+  const age3 = 10;
+
+  var person2 = {
+    name: "sanni",
+    age: 22,
+    getAgeArrow: ()=> console.log(this.age),
+    getAge: function () {
+      console.log(this.age);
+      
+    },
+    
+  };
+
+  var person3 = { age: 24};
+  person2.getAgeArrow.call(person3);
+  person2.getAge.call(person3);
